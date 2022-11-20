5c71edb37af6fabc305c2c0bb290b86ccdb0043f
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-11-20 19:19:01 -0200

    Create TasksList.tsx

---
+++ Desafio02-ignite/src/src/components/TasksList.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FlatList, FlatListProps } from 'react-native';
+import { EditTaskArgs } from '../pages/Home';
+
+import { ItemWrapper } from './ItemWrapper';
+import { TaskItem } from './TaskItem'
+export interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+interface TasksListProps {
+  tasks: Task[];
+  toggleTaskDone: (id: number) => void;
+  removeTask: (id: number) => void;
+  editTask: ({ taskId, newTitle }: EditTaskArgs) => void
+}
+
+export function TasksList({ tasks, editTask, toggleTaskDone, removeTask }: TasksListProps) {
+  return (
+    <FlatList
+      data={tasks}
+      keyExtractor={item => String(item.id)}
+      contentContainerStyle={{ paddingBottom: 24 }}
+      showsVerticalScrollIndicator={false}
+      renderItem={({ item, index }) => {
+        return (
+          <ItemWrapper index={index}>
+            <TaskItem
+              task={item}
+              toggleTaskDone={toggleTaskDone}
+              removeTask={removeTask}
+              editTask={editTask}
+            />
+          </ItemWrapper>
+        )
+      }}
+      style={{
+        marginTop: 32
+      }}
+    />
+  )
+}
