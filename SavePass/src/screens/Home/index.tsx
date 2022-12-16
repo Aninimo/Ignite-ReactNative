e7de71f532950aff2930b44574279ba344b04b81
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-12-16 08:14:51 -0200

    Create index.tsx

---
+++ SavePass/src/screens/Home/index.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useCallback } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
+
+import { Header } from '../../components/Header';
+import { SearchBar } from '../../components/SearchBar';
+import { LoginDataItem } from '../../components/LoginDataItem';
+
+import {
+  Container,
+  Metadata,
+  Title,
+  TotalPassCount,
+  LoginList,
+} from './styles';
+
+interface LoginDataProps {
+  id: string;
+  service_name: string;
+  email: string;
+  password: string;
+};
+
+type LoginListDataProps = LoginDataProps[];
+
+export function Home() {
+  const [searchText, setSearchText] = useState('');
+  const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
+  const [data, setData] = useState<LoginListDataProps>([]);
+
+  async function loadData() {
+    const dataKey = '@savepass:logins';
+    // Get asyncStorage data, use setSearchListData and setData
+    const storagedData = await AsyncStorage.getItem(dataKey)
+    const filteredData = data.filter(item => {
+      //strings do item e da busca passadas para lowerCase para uso de insensitive-case na busca
+      const lower = item.service_name.toLowerCase()
+      return lower.includes(searchText.toLowerCase())
+    })
+
+    if(searchText && filteredData){
+      setSearchListData(filteredData)
+    }else{
+      if(storagedData){
+        setSearchListData(JSON.parse(storagedData))
+        setData(JSON.parse(storagedData))
+      }
+    }
+  }
+
+  function handleFilterLoginData() {
+    // Filter results inside data, save with setSearchListData
+    setSearchText(text)
+  }
+
+  function handleChangeInputText(text: string) {
+    // Update searchText value
+    if(text === ''){
+      setData(searchListData)
+    } else{
+        setSearchText(text)
+      }
+  }
+
+  useFocusEffect(useCallback(() => {
+    loadData();
+  }, []));
+
+  return (
+    <>
+      <Header
+        user={{
+          name: 'Rocketseat',
+          avatar_url: 'https://i.ibb.co/ZmFHZDM/rocketseat.jpg'
+        }}
+      />
+      <Container>
+        <SearchBar
+          placeholder="Qual senha você procura?"
+          onChangeText={handleChangeInputText}
+          value={searchText}
+          returnKeyType="search"
+          onSubmitEditing={handleFilterLoginData}
+
+          onSearchButtonPress={handleFilterLoginData}
+        />
+
+        <Metadata>
+          <Title>Suas senhas</Title>
+          <TotalPassCount>
+            {searchListData.length
+              ? `${`${searchListData.length}`.padStart(2, '0')} ao total`
+              : 'Nada a ser exibido'
+            }
+          </TotalPassCount>
+        </Metadata>
+
+        <LoginList
+          keyExtractor={(item) => item.id}
+          data={searchListData}
+          renderItem={({ item: loginData }) => {
+            return <LoginDataItem
+              service_name={loginData.service_name}
+              email={loginData.email}
+              password={loginData.password}
+            />
+          }}
+        />
+      </Container>
+    </>
+  )
+}
